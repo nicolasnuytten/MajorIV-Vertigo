ae0cdedd2118156c94172bba4bdc39621397dcdf
jurgenpeuteman <jurgen.peuteman@student.howest.be>
2018-03-26 14:47:04 +0200

    corner progression bar

--- src/js/App.js
+++ src/js/App.js
@@ -7,24 +7,37 @@ const imgPath = `assets/img/`;
 let imageCount = 0;
 let firstTime = false;
 
+const cornerImage = document.querySelector(`.corner-image`);
+
 const init = () => {
   console.log(`hello world`);
   console.log(`hallo`);
   ReactDOM.render(<Test />, document.getElementById(`reactcontent`));
 
-
-
+  window.addEventListener(`scroll`, scrollFunc);
   setInterval(changeImage, 1000);
-
 };
 
+const scrollFunc = () => {
+  console.log(window.scrollY);
+  const scroll = window.scrollY;
+  if (scroll <= 25) {
+    cornerImage.src = `/assets/img/corners/corner_1.png`;
+  } else if (scroll <= 200) {
+    cornerImage.src = `/assets/img/corners/corner_2.png`;
+  } else if (scroll <= 400) {
+    cornerImage.src = `/assets/img/corners/corner_3.png`;
+  } else if (scroll <= 500) {
+    cornerImage.src = `/assets/img/corners/corner_4.png`;
+  }
+
+};
 
 const changeImage = () => {
   const oldImageCount = imageCount;
   if (imageCount >= 3) {
     imageCount = 0;
   }
-
   //console.log(`${oldImageCount} old`);
   imageCount ++;
   //console.log(`${imageCount} new`);
@@ -37,9 +50,6 @@ const changeImage = () => {
     }
   }
   firstTime = true;
-
-
-
 };
 
 
