88b689e175cc0a38b2d5279e674be7f4fb315dfe
jurgenpeuteman <jurgen.peuteman@student.howest.be>
2018-03-27 17:23:56 +0200

    feature implemented, but needs enhancements

--- src/js/App.js
+++ src/js/App.js
@@ -8,18 +8,69 @@ let firstTime = false;
 
 const cornerImage = document.querySelector(`.corner-image`);
 
+const tm2article1 = document.querySelector(`.tm2-article1`);
+const tm2article2 = document.querySelector(`.tm2-article2`);
+const tm2article3 = document.querySelector(`.tm2-article3`);
+const tm2img1 = document.querySelector(`.tm2-img2`);
+const tm2img2 = document.querySelector(`.tm2-img3`);
+const tm2img3 = document.querySelector(`.tm2-img4`);
+
+
 const init = () => {
+  setupLogs();
+  setupListeners();
+  setupIntroductionTimer();
+  setupReactElements();
+};
+
+const setupLogs = () => {
   console.log(`hello world`);
   console.log(`hallo`);
-  //ReactDOM.render(<Test />, document.getElementById(`reactcontent`));
-  window.addEventListener(`scroll`, scrollFunc);
+};
+
+const setupListeners = () => {
+  window.addEventListener(`scroll`, scrollCorner);
+  window.addEventListener(`scroll`, tm2scrollEffect);
+};
+
+const setupIntroductionTimer = () => {
   const interval = 10000; //miliseconden
   setInterval(changeImage, interval);
 };
 
-const scrollFunc = () => {
-  console.log(window.scrollY);
+const setupReactElements = () => {
+  //ReactDOM.render(<Test />, document.getElementById(`reactcontent`));
+};
+
+const tm2scrollEffect = () => {
   const scroll = window.scrollY;
+  if (scroll <= 3200) {
+    tm2article1.classList.remove(`tm2-articleEffect`);
+    tm2article2.classList.add(`tm2-articleEffect`);
+    tm2article3.classList.add(`tm2-articleEffect`);
+    tm2img1.classList.remove(`tm2-imgBGEffect`);
+    tm2img2.classList.add(`tm2-imgBGEffect`);
+    tm2img3.classList.add(`tm2-imgBGEffect`);
+  } else if (scroll <= 3300) {
+    tm2article1.classList.add(`tm2-articleEffect`);
+    tm2article2.classList.remove(`tm2-articleEffect`);
+    tm2article3.classList.add(`tm2-articleEffect`);
+    tm2img1.classList.add(`tm2-imgBGEffect`);
+    tm2img2.classList.remove(`tm2-imgBGEffect`);
+    tm2img3.classList.add(`tm2-imgBGEffect`);
+  } else if (scroll >= 3500) {
+    tm2article1.classList.add(`tm2-articleEffect`);
+    tm2article2.classList.add(`tm2-articleEffect`);
+    tm2article3.classList.remove(`tm2-articleEffect`);
+    tm2img1.classList.add(`tm2-imgBGEffect`);
+    tm2img2.classList.add(`tm2-imgBGEffect`);
+    tm2img3.classList.remove(`tm2-imgBGEffect`);
+  }
+};
+
+const scrollCorner = () => {
+  const scroll = window.scrollY;
+  //console.log(window.scrollY);
   if (scroll <= 2800) {
     cornerImage.src = `/assets/img/corners/corner_1.png`;
   } else if (scroll <= 4000) {
